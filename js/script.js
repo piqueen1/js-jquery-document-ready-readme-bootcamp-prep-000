8400c9f0f41b9908f8bbac0d1201edabb94ad605
Finn Thye <piqueen@gmail.com>
2019-05-05 17:07:08 +0000

    Done.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+  <script>
+    $("#text").append("This is so freaking cool.");
+  </script>
